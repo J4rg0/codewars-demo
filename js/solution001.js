9d406a3fcf924817029041f7bfe27f98418af6b5
Larin Pawluk <larin.pawluk@gmail.com>
2022-06-17 19:11:24 -0600

    FizzBuzz solution

--- js/solution001.js
+++ js/solution001.js
@@ -1,1 +1,11 @@
-alert('Working!')
+let number = 1
+while(number < 101) {
+    if(number % 3 === 0 && number % 5 === 0) {
+        console.log(`Fizzbuzz! ${number} is divisible by 3 and 5!`)
+    }else if(number % 3 === 0) {
+        console.log(`Fizz! ${number} is divisible by 3!`)
+    }else if(!(number % 3 === 0) && number % 5 === 0) {
+        console.log(`Buzz! ${number} is divisible by 5 but not 3!`)
+    }
+    number += 1  
+}
