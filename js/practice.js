057c8aa66888509657958ec12d597d6fabec3491
Larin Pawluk <larin.pawluk@gmail.com>
2022-06-30 11:09:57 -0600

    Practice JS

---
+++ js/practice.js
@@ -0,0 +1,7 @@
+function sayHello(firstName, lastName) {
+    const message = `Hello, ${firstName} ${lastName}!`;
+    document.querySelector("h1").innerText = message;
+  }
+let fName = prompt("What is your first name?")
+let lName = prompt("What is your last name?")
+sayHello(fName, lName)
