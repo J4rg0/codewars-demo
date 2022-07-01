556cec95b134a8b30c1ac8363326c6703fce8fc6
Larin Pawluk <larin.pawluk@gmail.com>
2022-07-01 11:13:30 -0600

    Further JS practice

--- js/practice.js
+++ js/practice.js
@@ -1,7 +1,23 @@
-function sayHello(firstName, lastName) {
-    const message = `Hello, ${firstName} ${lastName}!`;
-    document.querySelector("h1").innerText = message;
+// function sayHello(firstName, lastName) {
+//     const message = `Hello, ${firstName} ${lastName}!`;
+//     document.querySelector("h1").innerText = message;
+//   }
+// let fName = prompt("What is your first name?")
+// let lName = prompt("What is your last name?")
+// sayHello(fName, lName)
+
+// Square the given number x
+function square1(x) {
+    return Math.pow(x, 2);
   }
-let fName = prompt("What is your first name?")
-let lName = prompt("What is your last name?")
-sayHello(fName, lName)
+  
+  // Square the given number x
+  const square2 = x => Math.pow(x, 2)// TODO: complete the function code
+  
+  console.log(square1(0)); // Must show 0
+  console.log(square1(2)); // Must show 4
+  console.log(square1(5)); // Must show 25
+  
+  console.log(square2(0)); // Must show 0
+  console.log(square2(2)); // Must show 4
+  console.log(square2(5)); // Must show 25
