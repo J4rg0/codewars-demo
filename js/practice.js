5d4612b24fcb52f41d4364a3b38c20c235580a42
Larin Pawluk <larin.pawluk@gmail.com>
2022-07-03 11:33:00 -0600

    Further refinement of square root js

--- js/practice.js
+++ js/practice.js
@@ -6,14 +6,4 @@
 // let lName = prompt("What is your last name?")
 // sayHello(fName, lName)
 
-// Square the given number x
-function square1(x) {
-    for (let i = 0; i < x; i++) {
-        let square = Math.pow(i, 2);
-        console.log(`The square root of ${i} is ${square}`)
-  }
-}
-  // Square the given number x
-  const square2 = x => Math.pow(x, 2)// TODO: complete the function code
-  
-square1(10); 
+
---
+++ js/square-root.js
@@ -0,0 +1,17 @@
+// Square the given number x
+function square1(x) {
+    for (let i = 0; i < x; i++) {
+        let square = Math.pow(i, 2);
+        console.log(`The square root of ${i} is ${square}`)
+  }
+}
+  // Square the given number x
+  const square2 = x => {
+    let i = 0;
+    while (i < x) {
+        console.log(`The square root of ${i} is ${Math.pow(i, 2)}`);
+        i++;
+    }
+  }
+square1(10);
+square2(10); 
