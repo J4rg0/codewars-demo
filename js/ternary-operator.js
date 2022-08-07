f657b31bc1b22e442f0b2e495bba1cdc37be5c76
Larin Pawluk <larin.pawluk@gmail.com>
2022-08-07 17:30:17 -0600

    alternate ternary solution

--- js/ternary-operator.js
+++ js/ternary-operator.js
@@ -2,3 +2,5 @@ function saleHotdogs(n){
     // returns the total price of hotdogs depending on number of customers
     return n < 5 ? n * 100 : n >= 5 && n < 10 ? n * 95 : n * 90;
   }
+// single line version
+  const saleHotdogs=n=>n*(n<5?100:n<10?95:90);
