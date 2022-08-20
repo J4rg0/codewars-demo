e608594e25042b26e51573ba788b040e92fa8ae7
Larin Pawluk <larin.pawluk@gmail.com>
2022-08-20 15:03:24 -0600

    alternative toFixed solutions

--- js/toFixed.js
+++ js/toFixed.js
@@ -6,4 +6,23 @@ function howManySmaller(arr,n){
     }
     return count;
   }
+
+function howManySmaller(arr,n){
+
+    total = 0;
+    
+    for (i = 0; i < arr.length; i++){
+      
+      if (arr[i].toFixed(2) < n)
+      
+        total++;
+        
+    }
+    
+    return total;
+    
+}
   
+function howManySmaller(arr,n){
+    return arr.filter(x => +x.toFixed(2) < n).length;
+}
