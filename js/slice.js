f8f593af6759bc8a3c7940931f3811b9fa853962
Larin Pawluk <larin.pawluk@gmail.com>
2022-08-21 15:26:25 -0600

    Much more clever slice solution!

--- js/slice.js
+++ js/slice.js
@@ -11,3 +11,8 @@ function cutIt(arr){
     
   }
 
+// Clever!
+function cutIt(arr){
+    const minLength = Math.min(...arr.map(x => x.length));
+    return arr.map(x => x.slice(0, minLength));
+  }
