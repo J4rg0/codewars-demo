39238d519b6260a77f9129d6e2764321d8e587ee
Larin Pawluk <larin.pawluk@gmail.com>
2022-08-22 15:28:06 -0600

    alternate slice solution

--- js/slice.js
+++ js/slice.js
@@ -11,6 +11,19 @@ function cutIt(arr){
     
   }
 
+// alternate
+function cutIt(arr) {
+    var smallest = arr[0].length;
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i].length < smallest) {
+        smallest = arr[i].length;
+      }
+    }
+    var map = arr.map(function(x) {
+      return x.slice(0, smallest);
+    });
+    return map;
+  };
 // Clever!
 function cutIt(arr){
     const minLength = Math.min(...arr.map(x => x.length));
