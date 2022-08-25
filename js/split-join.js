d41f3a997b3d98afb9f6d145a02abce23cdb1cad
Larin Pawluk <larin.pawluk@gmail.com>
2022-08-25 16:01:49 -0600

    Alternative

--- js/split-join.js
+++ js/split-join.js
@@ -3,3 +3,15 @@ function splitAndMerge(str, sp) {
     // then joins each modified word with spaces
     return str.split(" ").map(word => word.split("").join(sp)).join(" ");
   }
+
+  function splitAndMerge(str,sp){
+    var words = str.split(" ");
+    var arr =[];
+    var res = []
+    for(i=0;i<words.length;i++){
+     arr.push(words[i].split("").join(sp));
+    }
+    res = arr.join(" ");
+    return res;
+    
+  }
