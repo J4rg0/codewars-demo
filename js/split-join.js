4f1ff9c1b4ec94d5630ed77df6948732f8c86084
Larin Pawluk <larin.pawluk@gmail.com>
2022-08-26 16:04:15 -0600

    single line split and join

--- js/split-join.js
+++ js/split-join.js
@@ -5,13 +5,20 @@ function splitAndMerge(str, sp) {
   }
 
   function splitAndMerge(str,sp){
+    // splits words into an array
     var words = str.split(" ");
     var arr =[];
     var res = []
+    // pushes each letter with the joining character to arr
     for(i=0;i<words.length;i++){
      arr.push(words[i].split("").join(sp));
     }
+    // joins the words in arr with spaces
     res = arr.join(" ");
     return res;
     
   }
+
+// single line
+
+splitAndMerge=(str,sp)=>str.split(' ').map(a=>a.split('').join(sp)).join(' ')
