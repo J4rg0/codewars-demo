8ae8bfced13bec5dc9db397c8b58ced1be425ab2
Larin Pawluk <larin.pawluk@gmail.com>
2022-09-01 21:11:49 -0600

    dna swap comments

--- js/dna-swap.js
+++ js/dna-swap.js
@@ -1,5 +1,7 @@
+// an object containing all possible pairs
 var pairs = {'A':'T','T':'A','C':'G','G':'C'};
 
 function DNAStrand(dna){
+  // returns a new array with the appropriate paired letter 
   return dna.split('').map(function(v){ return pairs[v] }).join('');
 }
