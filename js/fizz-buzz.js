215ca8a289a9fb833f42712dd4aa093a7e1e6568
Larin Pawluk <larin.pawluk@gmail.com>
2022-09-01 21:14:06 -0600

    Fizzbuzz alterations

--- js/fizz-buzz.js
+++ js/fizz-buzz.js
@@ -1,11 +1,14 @@
 let number = 1
 while(number < 101) {
+
     if(number % 3 === 0 && number % 5 === 0) {
         console.log(`Fizzbuzz! ${number} is divisible by 3 and 5!`)
     }else if(number % 3 === 0) {
         console.log(`Fizz! ${number} is divisible by 3!`)
     }else if(!(number % 3 === 0) && number % 5 === 0) {
         console.log(`Buzz! ${number} is divisible by 5 but not 3!`)
+    } else {
+        console.log(number)
     }
     number += 1  
 }
