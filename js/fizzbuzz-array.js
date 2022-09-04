1f393a87d5fd138a12035bf374b8565db801e978
Larin Pawluk <larin.pawluk@gmail.com>
2022-09-03 21:21:00 -0600

    Fizzbuzz array update

--- js/fizzbuzz-array.js
+++ js/fizzbuzz-array.js
@@ -4,9 +4,11 @@ function fizzbuzz(n)
   while(i <= n) {
     var fizz = (i % 3 == 0);
     var buzz = (i % 5 == 0);
+    // if fizz or buzz are true will push either or both to arr
     if(fizz || buzz) {
       arr.push((fizz?"Fizz":"") + (buzz?"Buzz":""));
     }
+    // otherwise will push the number
     else {
       arr.push(i);
     }
