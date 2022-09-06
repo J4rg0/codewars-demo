f297123bdef65fa0cc6e90e2da6eabb5878255bd
Larin Pawluk <larin.pawluk@gmail.com>
2022-09-05 21:29:44 -0600

    key values in objects

--- js/for-in.js
+++ js/for-in.js
@@ -1,6 +1,7 @@
 function giveMeFive(obj){
     var five=[];
     for (var key in obj) {
+      // will push either the key or value in obj if its length is 5
       if (key.length==5) five.push(key);
       if (obj[key].length==5) five.push(obj[key]);
     }
