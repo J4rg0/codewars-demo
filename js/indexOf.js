aebc6f8582863b6ccf1b317b0772f6ae26c70608
Larin Pawluk <larin.pawluk@gmail.com>
2022-09-11 22:00:14 -0600

    comments

--- js/indexOf.js
+++ js/indexOf.js
@@ -1,4 +1,6 @@
 function firstToLast(str,c){
+  // finds first and last occurence of c in str, returns the 
+  // difference between their locations or -1 if not in str
     if (str.search(c) > -1){
       return str.lastIndexOf(c) - str.indexOf(c)
     } else {
