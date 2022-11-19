97a766c6a9c7d62e2427f3781721c586aa1830df
Larin Pawluk <larin.pawluk@gmail.com>
2022-11-19 17:09:30 -0600

    loop methods solution

---
+++ js/loops.js
@@ -0,0 +1,17 @@
+function grabDoll(dolls){
+    var bag=[];
+    //coding here
+    for (let i=0; i<dolls.length;i++){
+      if (bag.length==3) {
+        break;
+      } else if (dolls[i] == 'Hello Kitty') {
+        bag.push(dolls[i]);
+      } else if (dolls[i] == 'Barbie doll') {
+        bag.push(dolls[i]);
+      } else {
+        continue;
+      }
+      
+    }
+    return bag;
+  }
